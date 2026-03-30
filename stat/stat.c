748f0a174c1a78cfdcb9101b1e9816ffddb07059
Razvan <razvangav1705@gmail.com>
2026-03-30 18:36:11 +0300

    Create stat.c

---
+++ stat/stat.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <sys/stat.h>
+#include <internal/syscall.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int stat(const char *restrict path, struct stat *restrict buf)
+{
+	return fstatat(AT_FDCWD, path, buf, 0);
+}
